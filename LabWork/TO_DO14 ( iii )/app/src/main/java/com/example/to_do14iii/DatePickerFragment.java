a8160479a1f2126bc8b3a528f93359d0a5579e23
livvy407 <ngawangyeshi1777@gmail.com>
2021-05-07 03:06:55 -0700

    TO_DO

--- LabWork/TO_DO14 ( iii )/app/src/main/java/com/example/to_do14iii/DatePickerFragment.java
+++ LabWork/TO_DO14 ( iii )/app/src/main/java/com/example/to_do14iii/DatePickerFragment.java
@@ -1,19 +1,27 @@
 package com.example.to_do14iii;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link DatePickerFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class DatePickerFragment extends Fragment {
+
+public class DatePickerFragment extends DialogFragment implements DatePickerDialog.OnDateSetListener {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -55,10 +63,19 @@ public class DatePickerFragment extends Fragment {
         }
     }
 
+    @NonNull
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_date_picker, container, false);
+    public Dialog onCreateDialog(@NonNull Bundle savedInstanceState){
+        final Calendar c = Calendar.getInstance();
+        int year = c.get(Calendar.YEAR);
+        int month = c.get(Calendar.MONTH);
+        int day = c.get(Calendar.DAY_OF_MONTH);
+        return new DatePickerDialog(getActivity(), this, year, month, day);
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+        MainActivity activity = (MainActivity) getActivity();
+        activity.processDatePickerResult(year, month, dayOfMonth);
     }
 }
